61612e5fe5f25600192bd67b3dcb74db532487dc
Ruutuli <rudhuli@gmail.com>
2025-12-17 18:00:54 -0500

    Add 'infinite-nodus' template type to worldTemplateMap
    - Included 'infinite-nodus' as a new template type in the worldTemplateMap function, expanding the available options for world slug handling.
    - This change enhances the flexibility of template type retrieval based on world slugs.

--- src/lib/templates/worldTemplateMap.ts
+++ src/lib/templates/worldTemplateMap.ts
@@ -16,6 +16,7 @@ export function getTemplateTypeFromWorldSlug(slug: string): string {
     'pluviophile': 'original',
     'tiderift': 'original',
     'vieulx': 'original',
+    'infinite-nodus': 'original',
     'none': 'none',
     'not-accessible': 'none',
   };
